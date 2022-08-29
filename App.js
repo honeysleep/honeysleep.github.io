cf0f460a7cb8683d55bf37754d8e25dfc8967287
honeysleep <111031348+honeysleep@users.noreply.github.com>
2022-08-29 17:55:14 +0900

    Create App.js

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
+import { Icon } from 'semantic-ui-react'
+import { Segment, Input } from 'semantic-ui-react'
+
+function App() {
+  return (
+
+    <div>
+<h1>Hello World</h1>
+<Icon loading name='spinner' />
+<Icon loading name='certificate' /><Icon loading name='asterisk' />
+
+<Segment inverted>
+  <Input inverted placeholder='Search...' />
+</Segment>
+
+<Progress percent = {55} indicating />
+<Button primary> 분석 해보기 </Button>
+
+
+
+    </div>
+
+  );
+}
+
+export default App;
